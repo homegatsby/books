5e98480f3ecf7a6e41438c4e4a202062f800f26a
homegatsby <syrno00@yandex.ru>
2020-08-10 17:07:50 +0800

    create models

--- bookstore/store/models.py
+++ bookstore/store/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    image = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='books')
+    tags = models.ManyToManyField(Tag, related_name='books')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
+
